6e3c6699a3642e1b0c69e8e337464f89a7eb77c4
dubongen <dubongen@hawaii.edu>
2018-09-16 02:01:18 -1000

    E26 Finished

---
+++ sumofnumbers.js
@@ -0,0 +1,43 @@
+const test = [1, 2, 3, 4, 5, 6];
+
+function sumFor(nums){
+  let total = 0;
+  for(const num of nums){
+    total += num;
+  }
+
+  return total;
+}
+
+console.log(sumFor(test));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length){
+    total += nums[i];
+    i++;
+  }
+  return total;
+  
+}
+
+console.log(sumWhile(test));
+
+function sumRecursion(nums) {
+  if (nums.length === 0){
+    return 0;
+  }
+
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(test));
+
+function simpleWay(nums) {
+  const num = _.reduce(nums, function(memo, num) {return memo + num;} , 0);
+  return num;
+
+}
+
+console.log(simpleWay(test));
